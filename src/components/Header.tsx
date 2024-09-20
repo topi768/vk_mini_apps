f733737146c10d0af2835772b1dfbbf7352ebbd0
topi768 <topi768@inbox.ru>
2024-09-20 16:22:55 +1000

    fix header back btn

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,8 +18,11 @@ export const Header: React.FC<PrestartModalProps> = ({ className, text }) => {
     <>
       <div className={className}>
         <header className="p-3 h-12 w-full flex align-center">
-          <button className=" left-0  flex justify-center items-center py-1 px-2 w-8 h-8 rounded-full bg-black  ">
-            <Icon12ChevronLeft className="text-white" onClick={onClick} />
+          <button
+            onClick={onClick}
+            className=" left-0  flex justify-center items-center py-1 px-2 w-8 h-8 rounded-full bg-black  "
+          >
+            <Icon12ChevronLeft className="text-white" />
           </button>
           <div className="w-full h-full flex justify-center items-center ">
             <h1 className="font-NauryzRedKeds text-black  text-[1.375rem] font-bold leading-[1.625rem] justify-center -translate-x-4">
