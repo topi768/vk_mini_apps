509e2bce5ff0df4933ffa2abc03aa993cc91f5de
topi768 <topi768@inbox.ru>
2024-09-24 10:56:48 +1000

    button push friend

--- src/components/ui/buttons/LargeButton.tsx
+++ src/components/ui/buttons/LargeButton.tsx
@@ -15,7 +15,7 @@ const LargeButton: React.FC<LargeButtonProps> = ({
   <div className={className}>
     <button
       onClick={onClick}
-      className={`flex justify-center items-center gap-2.5 p-2.5 p-2 w-[18.3125rem] rounded-full bg-primary  text-white  font-medium leading-[1.375rem] ${disabled ? "bg-btnDisabled" : "bg-primary"}`}
+      className={`flex justify-center items-center gap-2.5 p-2 w-[18.3125rem] rounded-full bg-primary  text-white  font-medium leading-[1.375rem] ${disabled ? "bg-btnDisabled" : "bg-primary"}`}
     >
       {text}
     </button>
