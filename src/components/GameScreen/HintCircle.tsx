3cafa97a395841fb09fd7fb84cbc645219033005
topi768 <topi768@inbox.ru>
2024-10-01 22:12:38 +1000

    fix hintCircle

--- src/components/GameScreen/HintCircle.tsx
+++ src/components/GameScreen/HintCircle.tsx
@@ -36,6 +36,7 @@ export const HintCircle: React.FC<HintCircleProps> = ({
     }
 
     setIsFirstOpen(false);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [countHints]);
 
   return (
